0b0733845118e8f9a4c5cfb99e911a39aac638cc
NamanCodes <95081384+Naman-sCode@users.noreply.github.com>
2023-09-04 15:36:25 +0530

    Add files via upload

---
+++ Project_1.py
@@ -0,0 +1,7 @@
+print("Welcome to the Band Name Generator")
+
+city = input("What is the name of your city?\n")
+
+pet = input("WHat is the name of your pet?\n")
+
+band_name = print("Your band name can be " + city + " " + pet)
